03a7912a4d3cd9cad7889413d68b3e246ceb3e47
rameshnaidu1999 <kasularameshnaidu@gmail.com>
2021-10-22 23:23:15 +0530

    create page also updated

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -13,6 +13,7 @@ import React, { useEffect, useState } from "react";
 import axios from "../config/axios";
 import PostCard from "../components/PostCard";
 import { useLocation } from "react-router-dom";
+import CreatePost from "./posts/CreatePost";
 
 // date.split("T")[0];
 
@@ -62,14 +63,14 @@ const Profile = () => {
       <Box mt={5}>
         <Paper className={classes.paper}>
           <Grid container spacing={3}>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={3}>
               <Avatar
                 alt="Remy Sharp"
                 src="/static/images/avatar/1.jpg"
                 className={classes.large}
               />
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={3}>
               <div className={classes.userdata}>
                 <Typography
                   gutterBottom
@@ -89,11 +90,16 @@ const Profile = () => {
                 </Typography>
               </div>
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={3}>
               <div className={classes.userdata}>
                 <Button variant="contained" color="secondary">
                   Edit Profile
                 </Button>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <div className={classes.userdata}>
+                <CreatePost title="Create Post" state={true} />
               </div>
             </Grid>
           </Grid>
@@ -112,7 +118,7 @@ const Profile = () => {
           </Typography>
           {posts && posts.length > 0 && (
             <Box mt={2}>
-              <Grid container spacing={10}>
+              <Grid container spacing={3}>
                 {posts.map((post) => (
                   <Grid item xs={12} sm={6} key={post._id}>
                     <PostCard post={post} location={location} />
